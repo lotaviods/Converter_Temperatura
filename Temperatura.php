1fcdd7aeb8cc6b3e277115a22ade43ce727ca5d2
lotaviods <l.otavio9099@gmail.com>
2020-10-28 11:19:26 -0300

    Mudanças_drasticas

---
+++ Temperatura.php
@@ -0,0 +1,23 @@
+<?php require 'Converter.php'; ?>
+<?php
+if ($_GET ['C'] == ''){
+    $TempF = new Converter();
+    $F = $_GET['F'];
+    $C = $TempF->FtoC($F);
+    echo "Em Celsius: $C <br /><br />";
+    ?>  
+    <?php
+    }elseif ($_GET ['F'] == ''){
+    $TempC = new Converter();
+    $C = $_GET['C'];
+    $F = $TempC->CtoF($C);
+    echo "Em Farenheit: $F <br /><br />";
+    }elseif ($_GET ['F'] == '' and $_GET ['C'] == ''){
+        echo "Vazio";
+    }else{
+        echo "Dois campos preenchidos";
+    }
+    ?>
+    <html> 
+    <?php
+?><form action = "Index.html"> <button type="submit" class="btn btn-primary">Voltar</button> </form> 
